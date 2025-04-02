6d48086a6660a093b2acfad22b00173198514baa
Salah Saleh <salahsaleh1015@gmail.com>
2025-04-02 15:17:11 +0200

    LayOut Builder

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,108 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MediaQueryTest());
+  runApp(const LayOutBuilderExample());
 }
 
-
-class MediaQueryTest extends StatelessWidget {
-  const MediaQueryTest({super.key});
+class LayOutBuilderExample extends StatelessWidget {
+  const LayOutBuilderExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    log(height.toString());
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: height*.2,
-              width: 200,
-              color: Colors.blue,
-            ),
-            Container(
-              height: height*.2,
-              width: 200,
-              color: Colors.black,
-            ),
-            Container(
-              height: height*.2,
-              width: 200,
-              color: Colors.red,
-            ),
-          ],
+        body: LayoutBuilder(
+          builder: (context , constraints) {
+            if(constraints.maxWidth < 600) {   // 600 is my break point
+              return MobileLayOut();
+            }else{
+              return DeskTopLayOut();
+            }
+
+          }
         ),
+      ),
+    );
+  }
+}
+
+class MobileLayOut extends StatelessWidget {
+  const MobileLayOut({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Container(
+          margin: EdgeInsets.only(bottom: 10),
+          color: Colors.lightBlue,
+          child: ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(number: index + 1),
+                ),
+              );
+            },
+            title: Text("Item ${index + 1}"),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key, required this.number});
+
+  final int number;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(number.toString(), style: TextStyle(fontSize: 50)),
+      ),
+    );
+  }
+}
+
+
+class DeskTopLayOut extends StatefulWidget {
+  const DeskTopLayOut({super.key});
+
+  @override
+  State<DeskTopLayOut> createState() => _DeskTopLayOutState();
+}
+
+class _DeskTopLayOutState extends State<DeskTopLayOut> {
+  int number = 1;
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Row(
+        children: [
+          Expanded(child:ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.only(bottom: 10),
+                color: Colors.lightBlue,
+                child: ListTile(
+               onTap: (){
+                 number = index + 1;
+                 setState(() {});
+               },
+                  title: Text("Item ${index + 1}"),
+                ),
+              );
+            },
+          ) ),
+          Expanded(child: Center(child: Text(number.toString(), style: TextStyle(fontSize: 50))))
+        ],
       ),
     );
   }
