bab96a6cab12d7a12e9ad1170b6b8fd94cf9b518
Salah Saleh <salahsaleh1015@gmail.com>
2025-04-02 19:15:33 +0200

    ..

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/main.
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+   // await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
