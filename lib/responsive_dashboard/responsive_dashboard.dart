d5df38d19a1ae5d2113157cdecda8d729e93e13d
Salah Saleh <salahsaleh1015@gmail.com>
2025-06-15 12:09:45 +0300

    dashboard init

---
+++ lib/responsive_dashboard/responsive_dashboard.dart
@@ -0,0 +1,17 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:mastering_flutter_responsive_and_adaptive_ui_design_course/responsive_dashboard/views/dashboard_view.dart' ;
+
+class ResponsiveDashboard extends StatelessWidget {
+  const ResponsiveDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashboardView(),
+    );
+  }
+}
